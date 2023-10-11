5b2620bdf4c80a2639a1b498365dab2b8e271960
fiqihmhayuda <fiqihmhayuda@gmail.com>
2023-10-11 19:07:48 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+// // js test
+
+// $(function(){
+
+// navbar ilang
+    var nav = document.querySelector('nav');
+
+        window.addEventListener('scroll', function() {
+        if (window.pageYOffset > 100) {
+          nav.classList.add('bs-dark-bg-subtle', 'shadow');
+        } else {
+         nav.classList.remove('bs-dark-bg-subtle', 'shadow');
+     }
+});
+
+// hamburger
+
+    // const hamburger = document.querySelector(".hamburger");
+    // const navbarToogler = document.querySelector(".navbar-toogler");
+
+    // hamburger.addEventListener("click", () => {
+    //     hamburger.classList.toggle("active");
+    //     navbarToogler.classList.toggle("active");
+    // })
+
+    // document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", ()=>{
+    //     hamburger.classList.remove("active");
+    //     navbarToogler.classList.remove("active");
+    // }))
+
+    const navbarCollapse = document.querySelector('#navbarNav');
+
+        // Add a click event listener to the navbar collapse
+    navbarCollapse.addEventListener('click', function() {
+        // Toggle the collapse state
+    navbarCollapse.classList.toggle('show');
+});
+        // When the navbar collapse is shown, add a class to the navbar
+    navbarCollapse.addEventListener('shown.bs.collapse', function() {
+        // Add the class
+    document.querySelector('.navbar').classList.add('open');
+});
+        // When the navbar collapse is hidden, remove the class from the navbar
+    navbarCollapse.addEventListener('hidden.bs.collapse', function() {
+        // Remove the class
+    document.querySelector('.navbar').classList.remove('open');
+});
+
+
+// text nya coba
+
+    var typed = new Typed(".typing", {
+        strings: ["Software Engineer", "Front-end Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+});
+
+// });
+
+// scrol top
+
+
